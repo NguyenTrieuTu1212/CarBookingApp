33d505cd3c213d8c50ab2a52200a6f96a4081a3f
NguyenTrieuTu1212 <143092693+NguyenTrieuTu1212@users.noreply.github.com>
2024-06-08 19:59:52 +0700

    Waiting API key google map

--- lib/Global/global_var.dart
+++ lib/Global/global_var.dart
@@ -4,9 +4,10 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 String userName ="";
 String userPhone="";
 String userID = FirebaseAuth.instance.currentUser!.uid;
-
+//AIzaSyAI9kPkskayYti5ttrZL_UfBlL3OkMEbvs
+//AIzaSyDuDxriw8CH8NbVLiXtKFQ2Nb64AoRSdyg
 String googleMapKey ="AIzaSyBbMNaYG54dqrE4TH03TETRp7MyXOCuuu8";
-String googeMapAPITest ="AIzaSyDuDxriw8CH8NbVLiXtKFQ2Nb64AoRSdyg";
+String googeMapAPITest ="AIzaSyAI9kPkskayYti5ttrZL_UfBlL3OkMEbvs";
 String serverKeyFCM ="AAAAHZmFRgM:APA91bEHPfHD4iZJJW485-somdn5EhzldAJ-Li5MhMC5YTcBVLWboNlL5fBv6T0kv7uHi-t0WkH9Shb-jTNJibEqoV9aow3ZOgY3NQnYYRh4r2CygZjjQPt8Owm4BhKf7lrJQiFwHGx7";
 
 //AIzaSyDuDxriw8CH8NbVLiXtKFQ2Nb64AoRSdyg
--- lib/Methods/common_methods.dart
+++ lib/Methods/common_methods.dart
@@ -112,7 +112,7 @@ class CommonMethods{
   static Future<String> convertGeoGraphicsIntoAddress(Position position,BuildContext context) async{
     String address = "";
     // another key API
-    String apiGeoUrl = "https://maps.googleapis.com/maps/api/geocode/json?latlng=${position.latitude},${position.longitude}&key=AIzaSyDuDxriw8CH8NbVLiXtKFQ2Nb64AoRSdyg";
+    String apiGeoUrl = "https://maps.googleapis.com/maps/api/geocode/json?latlng=${position.latitude},${position.longitude}&key=$googeMapAPITest";
     var responseFromApi = await sendRequestAPI(apiGeoUrl);
     if(responseFromApi != "Error"){
       address = responseFromApi["results"][0]["formatted_address"];
